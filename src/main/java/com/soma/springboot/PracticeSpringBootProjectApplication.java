a742394f1212f2ec2003be2e7bc54ed05fdf43be
soma <devSomaPro@gmail.com>
2018-09-08 19:54:07 +0900

    first commit

---
+++ src/main/java/com/soma/springboot/PracticeSpringBootProjectApplication.java
@@ -0,0 +1,12 @@
+package com.soma.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSpringBootProjectApplication.class, args);
+	}
+}
